038570bcb279b4d826dff52969e6d95f9e8146d5
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2021-12-09 12:15:16 +0300

    Add files via upload

---
+++ num2sr.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+	int a[50][50];
+	int i, j, n, m, x1, x2;
+	cout << " strok n= ";
+	cin >> n;
+	cout << " stolbcov m= ";
+	cin >> m;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			a[i][j] = rand() % 10;
+			cout << a[i][j]<<setw(3);
+
+		}
+		cout << endl;
+	}
+	cout << " x1= "; 
+
+}
